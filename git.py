7424787d9f2330ca695904eece42986059b7627f
svivoi <svivoi@mail.ru>
2019-05-16 17:07:30 +0700

    Изменил

---
+++ git.py
@@ -0,0 +1,8 @@
+#https://pypi.org/project/python-git/
+import pygit as g
+#g.update()
+g.repos()
+#repo = git.Repo.clone_from("git@github.com:user/project.wiki.git", "some-path")
+#repo.index.add(["index.html"])
+#repo.index.commit("Изменил index.html")
+#repo.remotes.origin.push()
